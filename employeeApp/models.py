dc5e45226d592a7451a364d4371ac0ec95d85537
Jithinreghunath <100864306+Jithinreghunath@users.noreply.github.com>
2022-12-07 18:26:12 +0530

    Add files via upload

---
+++ employeeApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class LeaveApplication(models.Model):
+#     L_Id=models.AutoField(primary_key=True)
+#     apply_Date = models.DateField()
+#     nature_Of_Leave = models.CharField(max_length=100)
+#     first_Day = models.DateField()
+#     last_Day = models.DateField()
+#     number_Of_Days = models.ImageField(max_length=100)
